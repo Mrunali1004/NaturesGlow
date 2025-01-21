842edfbee2554bbce1159f4f33ff47835bbbf37c
Mrunali1004 <zalavadiyamrunali@gmail.com>
2025-01-21 15:38:01 -0500

    Add login Page

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,11 +1,68 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+          Sign In to your account
+        </h2>
+      </div>
 
-export default Login
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form action="#" method="POST" className="space-y-6">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm/6 font-medium text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                required
+                autoComplete="email"
+                className="block w-full  bg-white px-3 py-1.5 text-base text-gray-900 border placeholder:text-gray-400 border-customButtonBorder sm:text-sm/6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Password
+              </label>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                required
+                autoComplete="current-password"
+                className="block w-full bg-white px-3 py-1.5 text-base text-gray-900 border placeholder:text-gray-400 border-customButtonBorder sm:text-sm/6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center bg-customButton px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
